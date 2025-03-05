a517b842109a8dbcbff22c9e4a363b6c618cd7bb
dave <justdavidthatsall@gmail.com>
2025-03-05 17:04:22 -0400

    more info update
    bigger file will just have to be searched thru server in the future, would not make much sense to do it any other way (already indexed the complete csv, can be used for very fast data pulls)

--- hub.js
+++ hub.js
@@ -1,9 +1,11 @@
 async function tableSearcher(gne, values, mutnumppl){
-  const groups = await fetch("p1p1.json"); //later change to "outputnobrackets" for full list on official site
+  // all info
+  const groups = await fetch("ch1editedforgithub.json"); //later change to "outputnobrackets" for full list on official site
   const groupData = await groups.json();
+
   var filterd = []
 
-  for (var i=0; i<groupData.length; i++){ //var i=0; i<groupData.length; i++  <- default - we're only running 1k for now as to not overload ->  var i=0; i<10000; i++
+  for (var i=0; i<10000; i++){ //var i=0; i<groupData.length; i++  <- default - we're only running 1k for now as to not overload ->  var i=0; i<10000; i++
     gne = gne
     values = values
     const valuespattern = new RegExp(String.raw`\b(?:${values.join('|')})\b`);
@@ -34,7 +36,7 @@ async function tableSearcher(gne, values, mutnumppl){
 }
 
 
-
+//builds the table
 function tableBuilder(data){
   function createRowThingy(name){
     let td = document.createElement('td');
@@ -64,6 +66,7 @@ function tableBuilder(data){
   }
 }
 
+//searching function
 async function seeker(){
   var q = document.getElementById("genesearchbox").value;
   var values = [];
@@ -84,6 +87,84 @@ async function seeker(){
       tableBuilder(data);
     });
   }
+};
+
+//listen for clicks on table, bring result when clicked
+document.addEventListener('DOMContentLoaded', function() {
+  const tbody = document.querySelector('#data-output');
+  tbody.addEventListener('click', function(event) {
+    const row = event.target.closest('tr');
+    if (row) {
+      const rowId = row.querySelector('td:nth-child(3)');
+      const rowIdReadable = rowId.textContent.trim();
+      console.log(`you clicked row with the id ${rowIdReadable}, also row = ${row.rowIndex}`);
+      //now we do some finding with "testcsvoutput.json", can possibly make an array and search like that but meh
+      fetch("testcsvoutput.json")
+        .then(Response => Response.json())
+        .then(jsonData => {
+          const foundData = findSNP(jsonData, rowIdReadable);
+          if (foundData) { //once data is found
+            console.log(foundData);
+            //create dropdown row, it ignores all cells and just displays text. all u want is text rn, no actual cells
+            // check if row exists, if so, delete it, if not, add it
+            if (document.getElementById(rowIdReadable)) {
+              var bye = document.getElementById(rowIdReadable).rowIndex;
+              tbody.deleteRow(bye - 1);
+            } else {
+              var tablelocation = document.getElementById("data-output");
+              var newrow = tablelocation.insertRow(row.rowIndex) //placing new row under clicked row
+              var cell1 = newrow.insertCell(0);
+              //clean up the text that's about to be shown
+              var texttobeshown =
+              `<strong>SNP:</strong> ${foundData.SNP}
+               <strong>CHR:</strong> ${foundData.CHR}
+               <strong>POS:</strong> ${foundData.POS}
+               <strong>ID:</strong> ${foundData.ID}
+               <strong>REF:</strong> ${foundData.REF}
+               <strong>ALT:</strong> ${foundData.ALT}
+               <strong>Effect:</strong> ${foundData.Effect}
+               <strong>AnyIsLof:</strong> ${foundData.AnyIsLof}
+               <strong>Deleterious:</strong> ${foundData.Deleterious}<br>
+               <strong>Gene:</strong> ${foundData.Gene}
+               <strong>FracTrAffected:</strong> ${foundData.FracTrAffected}
+               <strong>HGVSc:</strong> ${foundData.HGVSc}
+               <strong>HGVSp:</strong> ${foundData.HGVSp}<br>
+               <strong>N_HOM:</strong> ${foundData.N_HOM}
+               <strong>N_HET:</strong> ${foundData.N_HET}
+               <strong>N_REF:</strong> ${foundData.N_REF}<br>
+               <strong>AF:</strong> ${foundData.AF}
+               <strong>N_HOM_AA:</strong> ${foundData.N_HOM_AA}
+               <strong>N_HET_AA:</strong> ${foundData.N_HET_AA}
+               <strong>N_REF_AA:</strong> ${foundData.N_REF_AA}<br>
+               <strong>AF_AA:</strong> ${foundData.AF_AA}
+               <strong>N_HOM_EA:</strong> ${foundData.N_HOM_EA}
+               <strong>N_HET_EA:</strong> ${foundData.N_HET_EA}
+               <strong>N_REF_EA:</strong> ${foundData.N_REF_EA}<br>
+               <strong>AF_EA:</strong> ${foundData.AF_EA}
+               <strong>N_HOM_HA:</strong> ${foundData.N_HOM_HA}
+               <strong>N_HET_HA:</strong> ${foundData.N_HET_HA}
+               <strong>N_REF_HA:</strong> ${foundData.N_REF_HA}
+               <strong>AF_HA:</strong> ${foundData.AF_HA}
+              `
+              newrow.id = rowIdReadable //unique id
+              newrow.classList.add("dropdown");
+              cell1.colSpan = "13";
+              //cell1.rowSpan = "2";
+              cell1.innerHTML = `${texttobeshown}`;
+            }
+          } else { //cant find anything
+            console.log("cant find the matching snp");
+          }
+        })
+        .catch(error => {
+          console.log("Error fetching or parsing JSON:", error);
+        });
+    };
+  });
+});
+
+function findSNP(jsonData, snpValue) {
+  return jsonData.find(obj => obj.SNP === snpValue);
 };
 
 window.onload = function() {
--- src/tablesort.js
+++ src/tablesort.js
@@ -5,25 +5,3 @@
  * @param {boolean} asc if its ascending or not
  */
 //**
-
-/* function sorttablebycolumn(table, column, asc = true) {
-  const thedirection = asc ? 1 : -1;
-  const thetable = document.getElementById('example');
-  console.log(thetable);
-  const therows = Array.from(thetable.querySelectorAll("tr"));
-
-  //now we're sorting
-  const sortedrows = therows.sort((a, b) => {
-    const acolumntext = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
-    const bcolumntext = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
-
-    return acolumntext > bcolumntext ? (1*thedirection) : (-1*thedirection);
-  });
-}
-
-function timeee() {
-  sorttablebycolumn(document.querySelector("table"), 29);
-  setTimeout(timeee, 5000);
-}
-
-timeee(); IFFY CODE */
